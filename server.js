31296252d0e086e1e9b9924d4fadd4a4e7e9ca04
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-22 14:43:22 +1000

    Feat: style changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
+// const corsOptions = require('./config/corsOptions')
 const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
@@ -18,7 +18,7 @@ connectDB()
 
 app.use(logger)
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use(express.json())
 
