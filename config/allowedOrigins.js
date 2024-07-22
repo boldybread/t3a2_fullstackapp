48c836321981731e8e275c6d6bd6d5d92f4ee347
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-22 15:11:31 +1000

    Feat: origin cors fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,8 @@
 const allowedOrigins = [
-    'https://aperturescience.onrender.com/'
+    'https://aperturescience.onrender.com/',
+    Access-Control-Allow-Origin
 ]
+
+Access-Control-Allow-Origin;
 
 module.exports = allowedOrigins
