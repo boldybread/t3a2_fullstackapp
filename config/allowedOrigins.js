25b13b31eac75f5b3b2929810cf010265e119b81
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-07-22 15:56:45 +1000

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,18 +2,9 @@ const allowedOrigins = [
     'https://aperturescience.onrender.com/',
     'https://aperturescience-api.onrender.com/',
     'https://aperturescience-api.onrender.com/auth',
-    Access-Control-Allow-Origin *
+    Access-Control-Allow-Origin: *
 ]
 
-Access-Control-Allow-Origin;
+Access-Control-Allow-Origin: *
 
-const express = require('express');
-const corsOptions = {
-  origin: '*',
-};
-const app = express();
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.listen(config.PORT);
-
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
